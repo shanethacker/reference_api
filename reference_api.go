9892f8867511b1c400aceff64e68b587cb38b424
Shane Thacker <sst@clockworkmind.com>
2016-05-11 10:24:03 -0400

    Experimenting with setups per go-json-rest tutorial.

--- reference_api.go
+++ reference_api.go
@@ -1,16 +1,28 @@
 package main
 
 import (
-    "github.com/ant0ine/go-json-rest/rest"
-    "log"
-    "net/http"
+	"github.com/ant0ine/go-json-rest/rest"
+	"log"
+	"net"
+	"net/http"
 )
 
 func main() {
-    api := rest.NewApi()
-    api.Use(rest.DefaultDevStack...)
-    api.SetApp(rest.AppSimple(func(w rest.ResponseWriter, r *rest.Request) {
-        w.WriteJson(map[string]string{"Body": "Hello World!"})
-    }))
-    log.Fatal(http.ListenAndServe(":8080", api.MakeHandler()))
+	api := rest.NewApi()
+	api.Use(rest.DefaultDevStack...)
+	router, err := rest.MakeRouter(
+		rest.Get("/lookup/#host", func(w rest.ResponseWriter, req *rest.Request) {
+			ip, err := net.LookupIP(req.PathParam("host"))
+			if err != nil {
+				rest.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.WriteJson(&ip)
+		}),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	api.SetApp(router)
+	log.Fatal(http.ListenAndServe(":8080", api.MakeHandler()))
 }
